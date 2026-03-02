40262a035dd43023916f45a512f9faef93ee8cdf
Rohit's Projects <rohitsuthargecb@gmail.com>
2026-03-02 22:31:42 +0530

    Fixed Email Services

--- server/utils/sendemail.js
+++ server/utils/sendemail.js
@@ -3,9 +3,7 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.hostinger.com",
-      port: 465,
-      secure: true,
+     service: "Gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
